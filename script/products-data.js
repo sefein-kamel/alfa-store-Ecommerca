d595d4d97cc770ae94e737fb6d410bc59756ea32
sefein-kamel <sefoo.kamel.10@gmail.com>
2025-08-28 02:39:01 +0300

    add: Add products data page

---
+++ script/products-data.js
@@ -0,0 +1,97 @@
+const productsData = [
+  { id: 1,
+    name: "samsung s25 ultra",
+    category: "phone",
+    price: 78999,
+    desc: "Samsung Galaxy S25 Ultra AI Mobile Phone, 256GB Storage, 12GB RAM, Android Smartphone, 200MP Camera, S Pen, Long Battery Life, 1-Year Warranty",
+    img: "../images/sam-s25.jpg"
+},
+  { id: 2,
+    name: "samsung z fold7",
+    category: "phone",
+    price: 89950,
+    desc: "Samsung Galaxy Z Fold 7, AI Phone, Blue, 256GB Storage, 12GB Memory, 200MP Camera, 3nm Processor, 4400mAh Battery",
+    img: "../images/sam-zfold.jpg"
+},
+  { id: 3,
+    name: "iPhone 16 Plus",
+    category: "phone",
+    price: 60799,
+    desc: "Apple iPhone 16 Plus (128GB) - Green with Face ID (2-Year Warranty)",
+    img: "../images/iph-16plus.jpg"
+},
+  { id: 4,
+    name: "ASUS laptop",
+    category: "laptop",
+    price: 33899,
+    desc: "ASUS laptop, AMD processor, 14-inch screen, 8 GB RAM",
+    img: "../images/asus-lap.jpg"
+},
+  { id: 5,
+    name: "hp probook laptop",
+    category: "laptop",
+    price: 28999,
+    desc: "HP laptop, Intel processor, 15.6-inch screen, 8 GB RAM",
+    img: "../images/hp-lap.jpg"
+},
+  { id: 6,
+    name: "dell G15 laptop",
+    category: "laptop",
+    price: 37999,
+    desc: "Dell laptop, Intel processor, 15.6-inch screen, 16 GB RAM",
+    img: "../images/dell-lap.jpg"
+},
+  { id: 7,
+    name: "Shirt",
+    category: "clothes",
+    price: 399,
+    desc: "Modern Spanish Linen Shirt with Hood for Men",
+    img: "../images/shirt.jpg"
+},
+  { id: 8,
+    name: "T-Shirt",
+    category: "clothes",
+    price: 250,
+    desc: "Men's Long Sleeve T-Shirt - White",
+    img: "../images/t-shirt.jpg"
+},
+  { id: 9,
+    name: "Jeans",
+    category: "clothes",
+    price: 899,
+    desc: "Men's Sporty Jeans",
+    img: "../images/Jeans.jpg"
+},
+  { id: 10,
+    name: "Huawei Smartwatch",
+    category: "watch",
+    price: 5399,
+    desc: "Huawei Watch Fit 4 Smartwatch, 1.8-inch Display, 100+ Sports Modes, Fitness and Health Tracking, Emotional Health Assistant, Up to 10-Day Battery Life, Compatible with iOS and Android, Gray",
+    img: "../images/smart-watch.jpg"
+},
+  { id: 11,
+    name: "Classic watch",
+    category: "watch",
+    price: 1500,
+    desc: "Luxury Men's Stainless Steel Waterproof Watch",
+    img: "../images/classic-watch.jpg"
+},
+  { id: 12,
+    name: "sports watch",
+    category: "watch",
+    price: 2800,
+    desc: "Men's Digital Watch with Resin Strap Digital Watch with Black Strap",
+    img: "../images/sports-watch.jpg"
+},
+];
+
+
+  /*
+  { id: ,
+    name: "",
+    category: "",
+    price: ,
+    desc: "",
+    img: ""
+}
+*/
